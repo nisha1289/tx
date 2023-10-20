6414692cf84beeaaae153e5df64dfb60d4634630
nisha1289 <147962898+nisha1289@users.noreply.github.com>
2023-10-20 10:44:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 from telethon import TelegramClient
-api_id = 22274213
-api_hash = "53db38b5012cc9323a39c72b266855d3"
-bot_token = "5732523875:AAHZpUx0kXy5jcxIFA5Gj3g3CLTQFdJfSRw"
-skeleton_url = ""
+api_id = 22437328
+api_hash = "2f51bbe49cf796d04086ad882236a9db"
+bot_token = "6533903254:AAE-GdhuAHQZ0ihO2MB2_e694seEjDgMqzw"
+skeleton_url = "https://play.google.com/store/apps/details?id=com.utkarshnew.android"
 
 api_id = os.environ.get('API_ID')
 api_hash = os.environ.get('API_HASH')
